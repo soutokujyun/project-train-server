74cc82077912911cf71da4c17e26452c5f8ec65e
zengdexun <798795326@qq.com>
2021-12-10 01:05:59 +0800

    feat(app.model.user): 新增数据库 User

--- app/controller/user.js
+++ app/controller/user.js
@@ -2,7 +2,8 @@
 'use strict';
 
 const BaseController = require('./base.js');
-
+const md5 = require('md5');
+const HashSalt = ":zeng@good!@123";
 
 const createRules = {
   email: { type: 'string' },
@@ -30,8 +31,24 @@ class UserController extends BaseController {
     } else {
       // this.success({ name: 'kkb' });
       // 校验用户是否重复
+      if (await this.checkEmail(email)) {
+        this.error('邮箱重复了')
+      } else {
+        const ret = await ctx.model.User.create({
+          email,
+          nickname,
+          passwd: md5(passwd+HashSalt)
+        })
+        if (ret.id) {
+          this.message('注册成功')
+        }
+      }
     }
 
+  }
+  async checkEmail(email) {
+    const user = await this.ctx.model.User.findOne({email});
+    return user;
   }
   async verify() { }
   async info() { }
